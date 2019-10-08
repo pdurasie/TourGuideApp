349de953c147fbd92c8b702b05cc08704ffce0c6
Patrick Durasiewicz <pdurasie@pm.me>
2019-10-08 18:04:05 +0200

    feat: Finalize list contents of Historic tab

--- app/src/main/java/com/example/android/tourguideapp/FranzFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/FranzFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -25,7 +28,32 @@ public class FranzFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_franz, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fun_for_free, container, false);
+
+        ArrayList<Attraction> attractions = new ArrayList<>();
+        attractions.add(new Attraction(getString(R.string.landungsbr_location),
+                getString(R.string.landungsbr_desc),
+                R.drawable.landungsbr_pic));
+        attractions.add(new Attraction(getString(R.string.planten_location),
+                getString(R.string.planten_desc),
+                R.drawable.planten_pic));
+        attractions.add(new Attraction(getString(R.string.elbstrand_location),
+                getString(R.string.elbstrand_desc),
+                R.drawable.elbstrand_pic));
+        attractions.add(new Attraction(getString(R.string.buecherhallen_location),
+                getString(R.string.buecherhallen_desc),
+                R.drawable.buecherhallen_pic));
+        attractions.add(new Attraction(getString(R.string.stadtpark_location),
+                getString(R.string.stadtpark_desc),
+                R.drawable.stadtpark_pic));
+
+        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions);
+
+        ListView listView = rootView.findViewById(R.id.attraction_list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
--- app/src/main/java/com/example/android/tourguideapp/HistoricFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/HistoricFragment.java
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -25,7 +28,29 @@ public class HistoricFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_historic, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fun_for_free, container, false);
+
+        ArrayList<Attraction> attractions = new ArrayList<>();
+        attractions.add(new Attraction(getString(R.string.speicherstadt_location),
+                getString(R.string.speicherstadt_desc),
+                R.drawable.speicherstadt_pic));
+        attractions.add(new Attraction(getString(R.string.rathaus_location),
+                getString(R.string.rathaus_desc),
+                R.drawable.rathaus_pic));
+        attractions.add(new Attraction(getString(R.string.michel_location),
+                getString(R.string.michel_desc),
+                R.drawable.michel_pic));
+        attractions.add(new Attraction(getString(R.string.elbtunnel_location),
+                getString(R.string.elbtunnel_desc),
+                R.drawable.elbtunnel_pic));
+
+        AttractionAdapter adapter = new AttractionAdapter(getActivity(), attractions);
+
+        ListView listView = rootView.findViewById(R.id.attraction_list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
